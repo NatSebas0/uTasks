c50424706a88a905902acf1e17f2b51d6dfeac52
NatSebas <natsebas.2947.0218@gmail.com>
2024-09-04 10:14:15 -0500

    Avanze 3

--- Js/sidebar.js
+++ Js/sidebar.js
@@ -4,12 +4,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
-// Agregar funcionalidad para mantener el ícono activo
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function () {
-        document.querySelectorAll('.nav-link').forEach(link => {
-            link.classList.remove('active');
-        });
-        this.classList.add('active');
-    });
-});
